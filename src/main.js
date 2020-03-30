d007f7d575f2152552c4ddee9c1d3a978e273533
jinjing <705692274@qq.com>
2020-03-30 11:52:04 +0800

    完成项目的结构搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+//路由导入进来
+import router from './router/index'
+//导入ElementUI
+import ElementUI from 'element-ui'
+//导入ElementUI样式
+import 'element-ui/lib/theme-chalk/index.css';
+
+//使用ElementUI
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
 new Vue({
+  router, //将路由挂载在实例中
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+//导入vue
+import Vue from 'vue'
+//导入路由
+import VueRouter from 'vue-router'
+//使用vue-router
+Vue.use(VueRouter)
+//创建一个新的vue-router对象
+let router = new VueRouter({
+    //添加路由规则
+    routes: [
+        
+    ]
+})
+
+//将新的路由暴露出去
+export default router
